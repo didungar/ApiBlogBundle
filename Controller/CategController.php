8e1cc8cf43be3095698de9495af79563eb508cf0
Didier Ungar <didungar@gmail.com>
2017-01-22 13:07:33 +0100

    Possibilité de lister les categories

--- Controller/CategController.php
+++ Controller/CategController.php
@@ -6,19 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-/**
-* @Route("/categ")
-*/
 class CategController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/categ.json")
      * @Template()
      */
     public function indexAction()
     {
-        return $this->response([
-		
-	]);
+	$em = $this->getDoctrine()->getManager();
+	$repCateg = $em->getRepository('DidUngarBlogBundle:Categ');
+	$lstCateg = $repCateg->findBy([]);
+	foreach($lstCateg as $key => $oCateg) {
+		$lstCateg[$key] = [
+			'id'=>$oCateg->getId(),
+			'slug'=>$oCateg->getSlug(),
+			'name'=>$oCateg->getTitle(),
+			'order_cols'=>$oCateg->getOrderCols(),
+			'order_sens'=>$oCateg->getOrderSens(),
+		];
+	}
+
+        return $this->response($lstCateg);
     }
 }
